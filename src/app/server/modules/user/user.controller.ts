0de478428da4e8e5222818b444df246b130de91d
Fame Chutipong <chutipong.sae@gmail.com>
2026-08-10 10:29:41 +0700

    feat: create api CRUD user

--- src/app/server/modules/user/user.controller.ts
+++ src/app/server/modules/user/user.controller.ts
@@ -1,6 +1,9 @@
 import { UserService } from "./user.service";
 import { Context } from "hono";
 import { customLog } from "@/app/server/util/custom-log";
+import { HTTPException } from "hono/http-exception";
+import { convertErrorMessage } from "../../util/common";
+import { UserDto } from "../../dto/user.dto";
 
 export class UserController {
   constructor(
@@ -24,6 +27,100 @@ export class UserController {
           error: { message: error instanceof Error ? error.message : "user failed" },
         },
         400,
+      );
+    }
+  };
+
+  getAllUsers = async (c: Context) => {
+    try {
+      const users = await this.userService.getAllUsers();
+
+      return c.json({
+        success: true,
+        data: users,
+      });
+    } catch (error) {
+      customLog.error("Error getting all users", { error });
+      const status = error instanceof HTTPException ? error.status : 500;
+      return c.json(
+        {
+          success: false,
+          error: { message: error instanceof Error ? error.message : "Getting all users failed" },
+        },
+        status,
+      );
+    }
+  };
+
+  searchUsers = async (c: Context) => {
+    try {
+      const { username, department } = await c.req.json();
+      const users = await this.userService.searchUsers(username, department);
+
+      return c.json({
+        success: true,
+        data: users,
+      });
+    } catch (error) {
+      customLog.error("Error searching users", { error });
+      const status = error instanceof HTTPException ? error.status : 500;
+      return c.json(
+        {
+          success: false,
+          error: { message: error instanceof Error ? error.message : "Searching users failed" },
+        },
+        status,
+      );
+    }
+  };
+
+  updateUser = async (c: Context) => {
+    try {
+      const fullName = c.get("fullName");
+      const id = Number(c.req.param("id"));
+      const body: UserDto = await c.req.json();
+      const user = await this.userService.updateUser(id, body, fullName);
+
+      customLog.info("User updated :", { user });
+
+      return c.json({
+        success: true,
+        data: user,
+      }, 200);
+    } catch (error) {
+      customLog.error("Error updating user", { error });
+      const status = error instanceof HTTPException ? error.status : 500;
+      return c.json(
+        {
+          success: false,
+          error: { message: error instanceof Error ? convertErrorMessage(error.message) : "updating user failed" },
+        },
+        status,
+      );
+    }
+  };
+
+  deleteUser = async (c: Context) => {
+    try {
+      const fullName = c.get("fullName");
+      const id = Number(c.req.param("id"));
+      const user = await this.userService.deleteUser(id, fullName);
+
+      customLog.info("User deleted :", { user });
+
+      return c.json({
+        success: true,
+        message: "User deleted successfully",
+      }, 200);
+    } catch (error) {
+      customLog.error("Error deleting user", { error });
+      const status = error instanceof HTTPException ? error.status : 500;
+      return c.json(
+        {
+          success: false,
+          error: { message: error instanceof Error ? convertErrorMessage(error.message) : "deleting user failed" },
+        },
+        status,
       );
     }
   };
--- src/app/server/modules/user/user.repository.ts
+++ src/app/server/modules/user/user.repository.ts
@@ -2,6 +2,7 @@ import prismaInstance from "@/app/server/config/prismaClientInstance";
 import { customLog } from "@/app/server/util/custom-log";
 import { Prisma, User } from "@prisma/client";
 import { HTTPException } from "hono/http-exception";
+import { UserDto } from "../../dto/user.dto";
 
 export class UserRepository {
   private readonly prisma = prismaInstance;
@@ -15,8 +16,8 @@ export class UserRepository {
         },
       });
     } catch (error) {
-      customLog.error("Error fetching user", { error });
-      throw new HTTPException(400, { message: "Failed to fetch user" });
+      customLog.error("Error fetching user by ID", { error });
+      throw new HTTPException(400, { message: "Failed to fetch user by ID" });
     }
   }
 
@@ -31,17 +32,6 @@ export class UserRepository {
     }
   }
 
-  async findUserById(id: number): Promise<User | null> {
-    try {
-      return await this.prisma.user.findUnique({
-        where: { id },
-      });
-    } catch (error) {
-      customLog.error("Error fetching user by ID", { error });
-      throw new HTTPException(400, { message: "Failed to fetch user by ID" });
-    }
-  }
-
   async createUserTransaction(tx: Prisma.TransactionClient, userData: Prisma.UserCreateInput): Promise<User> {
     try {
       return await tx.user.create({
@@ -51,5 +41,150 @@ export class UserRepository {
       customLog.error("Error creating user", { error });
       throw new HTTPException(400, { message: "Failed to create user" });
     }
+  }
+
+  async getAllUser(): Promise<User[]> {
+    try {
+      return await this.prisma.user.findMany({
+        where: { 
+          isActive: true, 
+          isDeleted: false 
+        },
+      });
+    } catch (error) {
+      customLog.error("Error fetching users", { error });
+      throw new HTTPException(400, { message: "Failed to fetch users" });
+    }
+  }
+
+  async searchUsers(username?: string, department?: string): Promise<User[]> {
+    try {
+      const whereClause = this.buildWhereClause(username, department);
+      return await this.prisma.user.findMany({
+        where: { 
+          isActive: true, 
+          isDeleted: false,
+          ...whereClause
+        },
+      });
+    } catch (error) {
+      customLog.error("Error searching users", { error });
+      throw new HTTPException(400, { message: "Failed to search users" });
+    }
+  }
+
+  async updateUser(
+    id: number,
+    data: UserDto
+  ): Promise<User> {
+    try {
+      return await this.prisma.user.update({
+        where: { 
+          id, 
+          isDeleted: false 
+        },
+        data: {
+          ...(data.firstName !== undefined && {
+            firstName: data.firstName,
+          }),
+          ...(data.lastName !== undefined && {
+            lastName: data.lastName,
+          }),
+          ...(data.email !== undefined && {
+            email: data.email,
+          }),
+          ...(data.mobileNumber !== undefined && {
+            mobileNumber: data.mobileNumber,
+          }),
+          ...(data.departmentId !== undefined && {
+            departmentId: data.departmentId,
+          }),
+          updatedBy: data.updatedBy,
+          updatedAt: new Date(),
+        },
+      });
+    } catch (error) {
+      customLog.error("Error updating user", { error });
+      throw new HTTPException(400, { message: "Failed to update user" });
+    }
+  }
+
+  async deleteUser(
+    id: number,
+    updatedBy: string
+  ): Promise<User> {
+    try {
+      const updatedAt = new Date();
+      const user = await this.prisma.$transaction(async (tx) => {
+        await tx.auth.updateMany({
+          where: {
+            userId: id,
+          },
+          data: {
+            isDeleted: true,
+            updatedBy,
+            updatedAt,
+          },
+        });
+
+        return await tx.user.update({
+          where: {
+            id,
+          },
+          data: {
+            isDeleted: true,
+            updatedBy,
+            updatedAt,
+          },
+        });
+      });
+
+      return user;
+    } catch (error) {
+      customLog.error("Error deleting user", { error });
+      throw new HTTPException(400, { message: "Failed to delete user" });
+    }
+  }
+
+  private buildWhereClause(username?: string, department?: string): Prisma.UserWhereInput {
+    const whereClause: Prisma.UserWhereInput = {
+      isDeleted: false,
+    };
+
+    if (username) {
+      whereClause.OR = [
+        {
+          auth: {
+            some: {
+              username: {
+                contains: username,
+              },
+              isDeleted: false,
+            },
+          },
+        },
+        {
+          firstName: {
+            contains: username,
+          },
+        },
+        {
+          lastName: {
+            contains: username,
+          },
+        },
+      ];
+    }
+
+    if (department) {
+      whereClause.department = {
+        departmentName: {
+          contains: department,
+        },
+        isDeleted: false,
+      };
+    }
+
+    return whereClause;
   }
 }
